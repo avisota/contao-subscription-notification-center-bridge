18a25d91300e4bc8d7cf1c5d323e0454d207657c
Tristan Lins <tristan.lins@bit3.de>
2014-03-13 16:30:13 +0100

    Add translations for custom types.

---
+++ module/languages/en/tl_nc_notification.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-subscription-notification-center-bridge
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota-subscription']         = 'Avisota Subscription';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota_subscribe']            = 'Subscribe';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota_confirm_subscription'] = 'Confirm subscription';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota_unsubscribe']          = 'Unsubscribe';

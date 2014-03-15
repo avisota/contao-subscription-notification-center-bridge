15a14eb5a7671d11f8f461948665bc60bce30abf
Avibot <avibot@avisota.org>
2014-03-15 04:00:11 +0100

    Auto-Update translations from transifex

---
+++ module/languages/rm/tl_nc_notification.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-14T04:00:17+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota-subscription']         = 'Abunament dad Avisota';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota_confirm_subscription'] = 'Confermar l\'abunament';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota_subscribe']            = 'Abunar';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['avisota_unsubscribe']          = 'De-abunar';
+
